86692d6dc0d517c18568f268b38cbabb21304032
Kyle Hansen <khansen3@ncsu.edu>
2025-09-23 15:53:13 -0400

    most of ma hw2 done

--- ma580/hw2/hw2.py
+++ ma580/hw2/hw2.py
@@ -1,5 +1,5 @@
 import numpy
-import scipy
+# import scipy
 import scipy.sparse as sparse
 import matplotlib
 import matplotlib.pyplot as plt
@@ -51,6 +51,21 @@ def system_def(f, n):
 
     return poisson_system(x, y, A, q_vec)
 
+def hager_invnorm(A):
+    # Hager 1984, Higham 1988
+    n = A.shape[0]
+    x = (1/n)*numpy.ones((n))
+    while (True):
+        y = sparse.linalg.spsolve(A, x)
+        # heaviside instead of sign() so that sign(0) = 1
+        xi = 2*numpy.heaviside(y, 1) - 1 
+        z = sparse.linalg.spsolve(A,xi)
+        if (numpy.max(abs(z)) <= z@x):
+            return numpy.sum(abs(y))
+        x = numpy.zeros(n)
+        x[numpy.argmax(z)] = 1
+
+
 k = numpy.arange(3, 11, 1)
 n = 2**k
 h = 1/n
@@ -77,7 +92,9 @@ for ni in range(0, n.size):
     e[ni] = numpy.max(numpy.abs(K-U))
     # I = sparse.eye(A.shape[0], A.shape[1])
     # A_inv = sparse.linalg.spsolve(A, I)
-    # cond[ni] = sparse.linalg.onenormest(A)*sparse.linalg.onenormest(A_inv)
+    cond[ni] = sparse.linalg.onenormest(A)*hager_invnorm(A)
+    # cond[ni] = numpy.linalg.cond(A.toarray(), p=1)
+    print(cond[ni])
     if (ni>0):
         r[ni] = e[ni]/e[ni-1]
     if n[ni] == 2**10:
@@ -86,14 +103,38 @@ for ni in range(0, n.size):
         fig = plt.figure()
         ax = plt.axes(projection="3d")
         ax.plot_surface(x, y,(U-K), cmap=matplotlib.cm.Reds)
-        plt.title("Absolute Error, h="+str(h[ni]))
+        plt.title("Absolute Error, $h="+str(h[ni])+"$")
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
+        ax.set_zlabel("$f_c - f$")
         plt.show()
+        plt.savefig("abs_errs")
+        plt.close()
 
         fig = plt.figure()
         ax = plt.axes(projection="3d")
         ax.plot_surface(x, y,U, cmap=matplotlib.cm.Blues)
-        plt.title("Numerical solution, h="+str(h[ni]))
+        plt.title("Numerical solution, $h="+str(h[ni])+"$")
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$y$")
+        ax.set_zlabel("$f_c(x,y)$")
         plt.show()
+        plt.savefig("num_soln")
+        plt.close()
+
+fig = plt.figure()
+plt.scatter(n, cond)
+plt.title("Condition Number Estimates")
+plt.xlabel("$n$")
+plt.ylabel("$\\kappa_1(A)$, upper bound")
+plt.show()
+plt.savefig("condition_linear")
+plt.xscale("log")
+plt.yscale("log")
+plt.show()
+plt.savefig("condition_log")
+plt.close()
+
 
 print("h")
 print(h)
