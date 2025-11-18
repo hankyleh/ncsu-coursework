89cdd0736065d029f999b95b363eaba6d7eed65a
Kyle Hansen <khansen3@ncsu.edu>
2025-11-18 18:10:16 -0500

    hw4 complete

--- ne795/hw4/plot_omega.py
+++ ne795/hw4/plot_omega.py
@@ -13,19 +13,18 @@ def discrete_eig (l, st, c, dx, mu, w):
     sa = st*(1-c)
     tau = st*dx/mu
     alpha = (1/tau) - 1/(numpy.exp(tau)-1)
+    alph_p = 1 - 2*alpha
     beta = 0.5*st*dx*l
     a = 1/(3*st*dx)
-
-
     omega = 0*l
 
     for m in range(len(mu)):
-        print(m)
+        # print(m)
         omega = omega + (
             (
-                (c/2)*w[m]*((1/3)-mu[m]**2) *(1 - 4*alpha[m]*((mu[m]/(st*dx))-alpha[m])*numpy.tan(beta)**2) *(4*numpy.sin(beta)**2)
+                (c/2)*w[m]*((1/3)-mu[m]**2) *(1 + alph_p[m]*((2*mu[m]/(st*dx))+alph_p[m])* (numpy.tan(beta))**2)*(4*(numpy.sin(beta)**2))
             )/(
-                (st*dx)*(1+ 4* ((mu[m]/(st*dx))-alpha[m])**2 * numpy.tan(beta)**2     )*(4*a*numpy.sin(beta)**2 + sa*dx)
+                (1 + (((2*mu[m]/(st*dx))+alph_p[m])**2)*(numpy.tan(beta)**2)) * (sa*dx + 4*a*(numpy.sin(beta)**2))*(st*dx)
             )
         )
     omega = numpy.abs(omega)
@@ -36,11 +35,12 @@ def discrete_eig (l, st, c, dx, mu, w):
 
 # Continuous Fourier Modes
 c_vec = [0.1, 0.5, 0.9, 0.999, 1]
-l_vec = numpy.linspace(0.001, 20, 300)
+l_vec = numpy.linspace(0.001, 25, 600)
 
 
 
 c_vec_p2 = [0.5, 0.9, 1]
+l_vec_p2 = numpy.linspace(0.001, 25, 600)
 dx_vec = [0.01, 0.1, 1, 10, 100]
 
 [mu, w] = numpy.polynomial.legendre.leggauss(8)
@@ -50,7 +50,7 @@ print(w)
 
 
 continuous_omega = numpy.zeros((len(c_vec), len(l_vec)))
-discrete_omega = numpy.zeros((len(c_vec), len(l_vec)))
+discrete_omega = numpy.zeros((len(c_vec_p2), len(dx_vec), len(l_vec_p2)))
 
 sig_tot = 1.0
 
@@ -72,20 +72,36 @@ plt.legend()
 plt.title("Continuous Form Fourier Modes")
 plt.xlabel("$\\lambda$")
 plt.ylabel("$|\\omega$|")
+plt.title("Continuous Fourier Eigenvalues")
+plt.savefig("continuous.png")
 # plt.show()
 plt.close()
 
+spec_rad = numpy.zeros((len(c_vec_p2), len(dx_vec)))
+spec_eig = numpy.zeros((len(c_vec_p2), len(dx_vec)))
 
-for c in c_vec_p2:
+
+for c in range(len(c_vec_p2)):
     plt.figure
-    for dx in dx_vec:
+    for dx in range(len(dx_vec)):
         # bound = numpy.pi * dx * sig_tot
-        l_vec_p2 = numpy.linspace(0.001, 50, 1000)
-        plt.plot(l_vec_p2, discrete_eig(l_vec_p2, sig_tot, c, dx, mu, w), label=f"{dx}")
+        discrete_omega[c][dx] = discrete_eig(l_vec_p2, sig_tot, c_vec_p2[c], dx_vec[dx], mu, w)
+        plt.plot(l_vec_p2, discrete_eig(l_vec_p2, sig_tot, c_vec_p2[c], dx_vec[dx], mu, w), label=f"dx = {dx_vec[dx]}")
+        spec_rad[c][dx] = numpy.max(discrete_omega[c][dx])
+        spec_eig[c][dx] = l_vec_p2[discrete_omega[c][dx]==spec_rad[c][dx]]
+        plt.scatter(spec_eig[c][dx], spec_rad[c][dx], s=15)
+    print(f"for c={c_vec_p2[c]}")
+    print(spec_rad[c])
+    print(spec_eig[c])
     plt.legend()
     # plt.yscale("log")
+    plt.xlabel("$\\lambda$")
+    plt.ylabel("$|\\omega$|")
+    plt.title(f"SC, SN Discretized Fourier Eigenvalues, c={c_vec_p2[c]}")
+    plt.savefig(f"disc_{c_vec_p2[c]}.png")
     plt.show()
     plt.close()
+
 
 
 
